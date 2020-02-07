0c90dc7ecb842d971d325b2d0f8c65ad48815c5c
Andrew <Andrewkn09@Gmail.com>
2020-02-07 01:18:51 -0500

    implement insert author query

--- server/index.js
+++ server/index.js
@@ -2,15 +2,36 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
+const db = require('../database/database.js');
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../client/dist/')));
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Listening on port localhost:${PORT}`)
-})
 
 app.get('/test', (req, res) => {
   res.send('hello')
+})
+
+app.post('/books', async (req, res) => {
+  const {first, middle = null, last} = req.body;
+  try {
+    const authorKey = await db.one(
+      `
+      INSERT INTO authors (first, middle, last) VALUES ($1, $2, $3) 
+      `,
+      [first, middle, last]
+      )
+      console.log(authorKey)
+      res.send('success')
+    } catch (err) {
+      console.log(err)
+      res.send('fail')
+  }
+
+    
+})
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on port localhost:${PORT}`)
 })
