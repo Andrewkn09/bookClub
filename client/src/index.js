9165e001c997112dc1f541ef25a42376fe81b2e7
Andrew <Andrewkn09@Gmail.com>
2020-02-11 12:30:06 -0500

    implement browser router

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './app/app'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './app/app';
 
-
-ReactDOM.render(<App/>, document.getElementById('app'))
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('app')
+);
