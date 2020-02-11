65c62ce16e74b05aa81f47dcd570ad49ab4d2e61
Andrew <Andrewkn09@Gmail.com>
2020-02-10 23:54:40 -0500

    create checknotauthenticated fn

--- server/middleware.js
+++ server/middleware.js
@@ -1,8 +1,16 @@
 function checkAuthenticated(req, res, next) {
   //built in passport fn that checks if user is authenticated
   if (req.isAuthenticated()) {
-    return next()
+    return next() 
   } 
 
   res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    res.redirect('/')
+  } 
+
+  next();
 }
