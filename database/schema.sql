09117103bdcf8d711f4a19d0c68f9a2ec154238f
Andrew <Andrewkn09@Gmail.com>
2020-02-10 21:38:41 -0500

    change author table to only have name column

--- database/schema.sql
+++ database/schema.sql
@@ -3,16 +3,14 @@ DROP TABLE IF EXISTS authors CASCADE;
 CREATE TABLE authors
 (
   id serial PRIMARY KEY,
-  first varchar(20) NOT NULL,
-  middle varchar(20),
-  last varchar(20) NOT NULL
+  name varchar(50) UNIQUE NOT NULL
 );
 
-CREATE UNIQUE INDEX auth_3col_uni_idx ON authors (first, middle, last)
-WHERE middle IS NOT NULL;
+-- CREATE UNIQUE INDEX auth_3col_uni_idx ON authors (first, middle, last)
+-- WHERE middle IS NOT NULL;
 
-CREATE UNIQUE INDEX auth_2col_uni_idx ON authors (first, last)
-WHERE middle IS NULL;
+-- CREATE UNIQUE INDEX auth_2col_uni_idx ON authors (first, last)
+-- WHERE middle IS NULL;
 
 DROP TABLE IF EXISTS books;
 
@@ -21,6 +19,6 @@ CREATE TABLE books
   id serial PRIMARY KEY,
   title varchar(50) NOT NULL ,
   author int NOT NULL references authors(id),
-  date date NOT NULL
+  date date 
 );
 
