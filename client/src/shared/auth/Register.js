52c98fd2ba06161ed556cc9a62c43bcf13c7fee7
Andrew <Andrewkn09@Gmail.com>
2020-02-11 12:30:54 -0500

    implement login and register features

---
+++ client/src/shared/auth/Register.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react'
+import {hot} from 'react-hot-loader/root';
+
+function Register() {
+  const [form, setForm] = useState({name: '', email: '', password: ''})
+
+  const handleChange = e => {
+    e.persist();
+    setForm(prevState => {
+      return {
+        ...prevState,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(form)
+  }
+
+  const {name, email, password} = form
+  return(
+    <form onSubmit={handleSubmit}>
+      <label>name</label>
+      <input type='text' name='name' value={name} onChange={handleChange} required/>
+      <label>email</label>
+      <input type='text' name='email' value={email} onChange={handleChange} required/>
+      <label>password</label>
+      <input type='text' name='password' value={password} onChange={handleChange}required/>
+      <button>Submit</button>
+    </form>
+  ) 
+
+  
+}
+
+export default hot(Register)
+
