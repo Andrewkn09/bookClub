90313b123336949dc42afa76b182ef24a7386fcd
Andrew <Andrewkn09@Gmail.com>
2020-02-11 11:49:12 -0500

    add users table

--- database/schema.sql
+++ database/schema.sql
@@ -31,3 +31,12 @@ CREATE TABLE books
   date_added date DEFAULT CURRENT_DATE
 );
 
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users
+(
+  id serial PRIMARY KEY,
+  name varchar(50) NOT NULL,
+  email varchar(50) NOT NULL,
+  hashpass varchar(60) NOT NULL 
+)
