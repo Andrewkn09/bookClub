9643ca19135266f13688f831d150065f7de3b401
Andrew <Andrewkn09@Gmail.com>
2020-02-11 12:30:21 -0500

    add routes to register and login

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom'
 import {hot} from 'react-hot-loader/root';
-
+import Register from '../shared/auth/Register'
+import Login from '../shared/auth/Login'
 
 function App() {
   return (
     <div>
       <h1>Book Clubs</h1>
+
+      <Switch>
+        <Route path='/register' component={Register}/>
+        <Route path='/login' component={Login}/>
+      </Switch>
     </div>
   )
 }
