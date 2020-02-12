3c1c80511e404b2eb1831db3d51c4c9a5f525b60
Andrew <Andrewkn09@Gmail.com>
2020-02-11 23:57:42 -0500

    implement get books axios request

--- client/src/shared/utils/api.js
+++ client/src/shared/utils/api.js
@@ -1,15 +1,13 @@
 import axios from 'axios';
 
-export const handleRegister =  async (user) => {
-  return await axios.post('/api/register', user)
-}
+export const handleRegister = async user => {
+  return await axios.post('/api/register', user);
+};
 
-export const handleLogin =  async (user) => {
-  return  await axios.post('/api/login', user)
-}
+export const handleLogin = async user => {
+  return await axios.post('/api/login', user);
+};
 
-
-
-
-
-
+export const fetchBooks = async () => {
+  return await axios.get('/books');
+};
