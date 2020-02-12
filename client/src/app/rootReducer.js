157478496208e93763e5c7a3e24ebbdeb4d96459
Andrew <Andrewkn09@Gmail.com>
2020-02-12 17:31:41 -0500

    add authreducer to state

--- client/src/app/rootReducer.js
+++ client/src/app/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import booksAdded from '../features/BookLibrary/bookLibraryReducer.js';
+import authReducer from '../shared/auth/authReducer.js';
 
 const rootReducer = combineReducers({
   books: booksAdded,
+  user: authReducer,
 });
 
 export default rootReducer;
