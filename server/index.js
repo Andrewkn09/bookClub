8a6805f30396e5d0f2458a5fc9a17f0874c1515a
Andrew <Andrewkn09@Gmail.com>
2020-02-12 17:37:11 -0500

    make book routes url api/books

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../client/dist/')));
 
 app.use('/', authRoutes);
-app.use('/books', bookRoutes);
+app.use('/api/books', bookRoutes);
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/'));
