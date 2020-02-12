e79e639a4a7f3da641a8118ddb664fdcd58e1a4c
Andrew <Andrewkn09@Gmail.com>
2020-02-12 18:34:18 -0500

    remove hotloader

--- client/src/shared/auth/Login.js
+++ client/src/shared/auth/Login.js
@@ -1,50 +1,64 @@
-import React, {useState} from 'react'
-import {hot} from 'react-hot-loader/root';
-
-import {handleLogin} from '../utils/api'
+import React, { useState } from 'react';
+import { handleLogin } from '../utils/api';
 
 function Login() {
-  const [form, setForm] = useState({name: '', email: '', password: ''})
-  const [error, updateError] = useState('')
+  const [form, setForm] = useState({ name: '', email: '', password: '' });
+  const [error, updateError] = useState('');
 
   const handleChange = e => {
     e.persist();
     setForm(prevState => {
       return {
         ...prevState,
-        [e.target.name]: e.target.value
-      }
-    })
-  }
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async e => {
     e.preventDefault();
-    
+
     try {
       e.preventDefault();
-      await handleLogin(form)
-      window.location.assign('/')
-
+      await handleLogin(form);
+      window.location.assign('/');
     } catch (err) {
-      console.log(err.response.data)
-      updateError(err.response.data.message)
+      console.log(err.response.data);
+      updateError(err.response.data.message);
     }
-  }
+  };
 
-  const {name, email, password} = form
-  return(
+  const { name, email, password } = form;
+  return (
     <form onSubmit={handleSubmit}>
       {error}
       <label>name</label>
-      <input type='text' name='name' value={name} onChange={handleChange} required/>
+      <input
+        type='text'
+        name='name'
+        value={name}
+        onChange={handleChange}
+        required
+      />
       <label>email</label>
-      <input type='text' name='email' value={email} onChange={handleChange} required/>
+      <input
+        type='text'
+        name='email'
+        value={email}
+        onChange={handleChange}
+        required
+      />
       <label>password</label>
-      <input type='text' name='password' value={password} onChange={handleChange}required/>
+      <input
+        type='text'
+        name='password'
+        value={password}
+        onChange={handleChange}
+        required
+      />
       <button>Submit</button>
     </form>
-  ) 
+  );
 }
 
-export default hot(Login)
-
+export default Login;
