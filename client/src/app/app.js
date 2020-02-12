d28aaa029a022b1a5545848d98ddc83b348f7469
Andrew <Andrewkn09@Gmail.com>
2020-02-12 00:12:29 -0500

    fetch books on load

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -1,21 +1,28 @@
-import React from 'react';
-import {Switch, Route} from 'react-router-dom'
-import Books from '../features/BookLibrary/BookLibrary.js'
-import Register from '../shared/auth/Register'
-import Login from '../shared/auth/Login'
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { Switch, Route } from 'react-router-dom';
+import Books from '../features/BookLibrary/BookLibrary.js';
+import Register from '../shared/auth/Register';
+import Login from '../shared/auth/Login';
+import * as actions from '../features/BookLibrary/bookLibraryActions';
 
-function App() {
+function App(props) {
+  useEffect(() => {
+    props.booksAdded();
+  }, []);
+
   return (
     <div>
       <h1>Book Clubs</h1>
 
       <Switch>
-        <Route exact path='/' component={Books}/>
-        <Route exact path='/register' component={Register}/>
-        <Route exact path='/login' component={Login}/>
+        <Route exact path='/' component={Books} />
+        <Route exact path='/register' component={Register} />
+        <Route exact path='/login' component={Login} />
       </Switch>
     </div>
-  )
+  );
 }
 
-export default App
+// export default App;
+export default connect(null, actions)(App);
