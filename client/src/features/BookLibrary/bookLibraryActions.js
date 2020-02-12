d9f43179df23df4d2a28926d79f8e6da0a56b6d8
Andrew <Andrewkn09@Gmail.com>
2020-02-12 00:13:18 -0500

    fix action to fetch books

--- client/src/features/BookLibrary/bookLibraryActions.js
+++ client/src/features/BookLibrary/bookLibraryActions.js
@@ -1,12 +1,11 @@
 import { BOOKS_ADDED } from '../../shared/types.js';
 import { fetchBooks } from '../../shared/utils/api.js';
 
-export const booksAdded = books => async dispatch => {
+export const booksAdded = () => async dispatch => {
   let books = await fetchBooks();
-  console.log(books);
 
   dispatch({
     type: BOOKS_ADDED,
-    payload: books,
+    payload: books.data,
   });
 };
