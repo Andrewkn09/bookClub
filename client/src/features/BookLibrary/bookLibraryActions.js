e59ebb410fbd445a81b07d6423365980df330991
Andrew <Andrewkn09@Gmail.com>
2020-02-12 17:21:05 -0500

    fix import location

--- client/src/features/BookLibrary/bookLibraryActions.js
+++ client/src/features/BookLibrary/bookLibraryActions.js
@@ -1,11 +1,15 @@
-import { BOOKS_ADDED } from '../../shared/types.js';
+import { BOOKS_ADDED } from '../../shared/utils/types.js';
 import { fetchBooks } from '../../shared/utils/api.js';
 
 export const booksAdded = () => async dispatch => {
-  let books = await fetchBooks();
+  try {
+    let books = await fetchBooks();
 
-  dispatch({
-    type: BOOKS_ADDED,
-    payload: books.data,
-  });
+    dispatch({
+      type: BOOKS_ADDED,
+      payload: books.data,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
