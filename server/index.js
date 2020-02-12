f61902b0c152806e513c982a5d3223d6f6cef763
Andrew <Andrewkn09@Gmail.com>
2020-02-12 17:08:28 -0500

    move book routes into separate routes file

--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
-const db = require('../database/database.js');
 
 const cookieSession = require('cookie-session');
 const passport = require('passport');
@@ -14,7 +13,7 @@ const passport = require('passport');
 require('./passport-config.js')();
 
 const authRoutes = require('./routes/authRoutes.js');
-const { isAuthenticated } = require('./middleware.js');
+const bookRoutes = require('./routes/bookRoutes.js');
 
 // const session = require('express-session')
 // app.use(session({
@@ -35,64 +34,14 @@ app.use(
   })
 );
 
-//sets up passport config
-// initilizePassport()
 //passport fn that sets up basics need to start
 app.use(passport.initialize());
-//works with app.use session, store variables to be persisted across entire session
 app.use(passport.session());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../client/dist/')));
+
 app.use('/', authRoutes);
-
-app.get('/books', isAuthenticated, async (req, res) => {
-  const { id } = req.user;
-  try {
-    const bookList = await db.many(`SELECT * FROM books WHERE userId = $1`, [
-      id,
-    ]);
-
-    res.send({ favorites: bookList });
-  } catch (err) {
-    console.log(err);
-    res.sendStatus(500);
-  }
-});
-
-app.post('/books', async (req, res) => {
-  const { title, author, genre, userid, notes } = req.body;
-
-  try {
-    //obtain author key, add if doesn't exist
-    const {
-      id: authorKey,
-    } = await db
-      .one(`INSERT INTO authors (name) VALUES ($1) RETURNING id`, [author])
-      .catch(err => {
-        return db.one(`SELECT id FROM authors WHERE name=$1 `, [author]);
-      });
-
-    //obtain genre key, add if doesn't exist
-    const {
-      id: genreKey,
-    } = await db
-      .one(`INSERT INTO genres (genre) VALUES ($1) RETURNING id`, [genre])
-      .catch(err => {
-        return db.one(`SELECT id from genres WHERE genre=$1`, [genre]);
-      });
-
-    //add book into database
-    await db.none(
-      `INSERT INTO books (title, author, genre, userid, notes) VALUES ($1, $2, $3, $4, $5)`,
-      [title, authorKey, genreKey, userid, notes]
-    );
-
-    res.send('success');
-  } catch (err) {
-    console.log(err);
-    res.sendStatus(500);
-  }
-});
+app.use('/books', bookRoutes);
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/'));
