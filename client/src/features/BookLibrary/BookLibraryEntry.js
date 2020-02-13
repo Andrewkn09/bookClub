615bc4ca7a7a9f7c398c1caddb5c64fc5e3c7cf7
Andrew <Andrewkn09@Gmail.com>
2020-02-12 23:12:54 -0500

    Implement book library entry form

---
+++ client/src/features/BookLibrary/BookLibraryEntry.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+const BookLibraryEntry = props => {
+  const [book, updateBook] = useState({});
+
+  const handleChange = e => {
+    e.persist();
+    updateBook(prevState => {
+      return { ...prevState, [e.target.name]: e.target.value };
+    });
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    try {
+      await props.postBookAndUpdate(book);
+      //TODO:
+      //if bookadded.loaded is true -> close dialog, pass handleCloseDialog
+      //if bookadded.err is true -> show error
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type='text'
+        name='title'
+        value={book.title || ''}
+        onChange={handleChange}
+      />
+      <input
+        type='text'
+        name='author'
+        value={book.author || ''}
+        onChange={handleChange}
+      />
+      <input
+        type='text'
+        name='genre'
+        value={book.genre || ''}
+        onChange={handleChange}
+      />
+      <textarea name='notes' value={book.notes || ''} onChange={handleChange} />
+      <button>Submit</button>
+    </form>
+  );
+};
+
+export default BookLibraryEntry;
