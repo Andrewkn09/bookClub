7638aaa49029cb6ee47523870d48df3c26b21479
Andrew <Andrewkn09@Gmail.com>
2020-02-13 16:23:05 -0500

    Implement bookupdated actions

--- client/src/features/BookLibrary/bookLibraryActions.js
+++ client/src/features/BookLibrary/bookLibraryActions.js
@@ -3,8 +3,11 @@ import {
   BOOK_POSTED_REQUEST,
   BOOK_POSTED_SUCCESS,
   BOOK_POSTED_FAILURE,
+  BOOK_UPDATED_REQUEST,
+  BOOK_UPDATED_SUCCESS,
+  BOOK_UPDATED_FAILURE,
 } from '../../shared/utils/types.js';
-import { fetchBooks, postBook } from '../../shared/utils/api.js';
+import { fetchBooks, postBook, updateBook } from '../../shared/utils/api.js';
 
 export const booksFetched = () => async dispatch => {
   try {
@@ -39,5 +42,34 @@ export const bookPosted = book => async dispatch => {
       type: BOOK_POSTED_FAILURE,
       payload: err,
     });
+  }
+};
+
+const bookRequest = (book, request, success, failure) => ({
+  type: request,
+  payload: book,
+});
+
+const bookRequestSuccess = book => ({
+  type: success,
+  payload: book,
+  loading: false,
+  loaded: true,
+  lastUpdated: Date.now(),
+});
+
+const bookRequestFailure = (book, err) => ({
+  type: failure,
+  payload: book,
+  error: err,
+});
+
+export const bookUpdated = book => async dispatch => {
+  dispatch(bookRequest(book, BOOK_UPDATED_REQUEST));
+  try {
+    await updateBook(book);
+    dispatch(bookRequestSuccess(book, BOOK_UPDATED_SUCCESS));
+  } catch (err) {
+    dispatch(bookRequestFailure(err, BOOK_UPDATED_FAILURE));
   }
 };
