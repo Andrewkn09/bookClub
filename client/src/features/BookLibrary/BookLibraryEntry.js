78abe37edf902b182a9479afa8be39159beff7ae
Andrew <Andrewkn09@Gmail.com>
2020-02-13 16:22:11 -0500

    Make submit conditionally call update or post book

--- client/src/features/BookLibrary/BookLibraryEntry.js
+++ client/src/features/BookLibrary/BookLibraryEntry.js
@@ -2,19 +2,22 @@ import React, { useState } from 'react';
 
 const BookLibraryEntry = props => {
   const [book, updateBook] = useState(props.initialState || {});
-  console.log('initial', props.initialState);
 
   const handleChange = e => {
     e.persist();
+
     updateBook(prevState => {
       return { ...prevState, [e.target.name]: e.target.value };
     });
   };
 
+  //if book has id, it means update. If not it means post
   const handleSubmit = async e => {
     e.preventDefault();
     try {
-      await props.postBookAndUpdate(book);
+      book.id
+        ? await props.updateBookandUpdate(book)
+        : await props.postBookAndUpdate(book);
       //TODO:
       //if bookadded.loaded is true -> close dialog, pass handleCloseDialog
       //if bookadded.err is true -> show error
