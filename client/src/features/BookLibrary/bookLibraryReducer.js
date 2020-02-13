e5ec17198d7fd5dc28e161d1e19c6943d26ea052
Andrew <Andrewkn09@Gmail.com>
2020-02-12 23:11:01 -0500

    Implement book posted reducer

--- client/src/features/BookLibrary/bookLibraryReducer.js
+++ client/src/features/BookLibrary/bookLibraryReducer.js
@@ -1,6 +1,11 @@
-import { BOOKS_FETCHED } from '../../shared/utils/types.js';
+import {
+  BOOKS_FETCHED,
+  BOOK_POSTED_REQUEST,
+  BOOK_POSTED_SUCCESS,
+  BOOK_POSTED_FAILURE,
+} from '../../shared/utils/types.js';
 
-const booksAdded = (state = [], { type, payload }) => {
+export const booksFetched = (state = [], { type, payload }) => {
   switch (type) {
     case BOOKS_FETCHED:
       return payload;
@@ -9,4 +14,35 @@ const booksAdded = (state = [], { type, payload }) => {
   }
 };
 
-export default booksAdded;
+const initialState = {
+  loading: false,
+  loaded: false,
+  error: null,
+};
+
+export const bookPosted = (state = initialState, action) => {
+  switch (action.type) {
+    case BOOK_POSTED_REQUEST:
+      return Object.assign({}, state, {
+        loading: true,
+        loaded: false,
+      });
+
+    case BOOK_POSTED_SUCCESS:
+      return Object.assign({}, state, {
+        loading: false,
+        loaded: true,
+        lastUpdated: Date.now(),
+      });
+
+    case BOOK_POSTED_FAILURE:
+      return Object.assign({}, state, {
+        loading: false,
+        loaded: false,
+        error: action.payload,
+      });
+
+    default:
+      return state;
+  }
+};
