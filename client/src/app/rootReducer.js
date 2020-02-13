65cede19d5333f99adf92859f863b0a41bbe9a00
Andrew <Andrewkn09@Gmail.com>
2020-02-12 23:11:33 -0500

    Change bookAdded to bookPosted

--- client/src/app/rootReducer.js
+++ client/src/app/rootReducer.js
@@ -1,14 +1,14 @@
 import { combineReducers } from 'redux';
 import {
   booksFetched,
-  bookAdded,
+  bookPosted,
 } from '../features/BookLibrary/bookLibraryReducer.js';
 import authReducer from '../shared/auth/authReducer.js';
 
 const rootReducer = combineReducers({
   books: booksFetched,
   user: authReducer,
-  bookAdded: bookAdded,
+  bookAdded: bookPosted,
 });
 
 export default rootReducer;
