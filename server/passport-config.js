8ca64ce1612499a0f96c2a120c09a186fde9ae82
Andrew <Andrewkn09@Gmail.com>
2020-02-13 15:23:48 -0500

    Add error handling for deserializeUser

--- server/passport-config.js
+++ server/passport-config.js
@@ -1,56 +1,61 @@
-const passport = require('passport')
-const LocalStrategy = require('passport-local').Strategy
-const bcrypt = require('bcrypt')
-const db = require('../database/database.js')
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const db = require('../database/database.js');
 
 function initilizePassport() {
   //email and passpord gets obtained from req.body
   const authenticateUser = async (email, password, done) => {
     try {
       //fn to obtail user object, obtain from model, need user to pass serialize again and compare hash with hashpass
-      const user = await db.oneOrNone(`SELECT * FROM users WHERE email = $1`, [email])
-      
+      const user = await db.oneOrNone(`SELECT * FROM users WHERE email = $1`, [
+        email,
+      ]);
+
       //check if user exists
       if (!user) {
         //1st param = the error, 2nd param = user found, 3rd param = message, appears as info when calling passport.authenticate
-        return done(null, false, {message: 'No user with that email'})
+        return done(null, false, { message: 'No user with that email' });
       }
 
       //check if password matches
-      if (!await bcrypt.compare(password, user.hashpass)) {
-        return done(null, false, {message: 'Password incorrect'})
+      if (!(await bcrypt.compare(password, user.hashpass))) {
+        return done(null, false, { message: 'Password incorrect' });
       }
 
       //suppllies passport with user to log in when calling passport.authenticate
-      return done(null, user)
-
+      return done(null, user);
     } catch (err) {
       console.log(err);
-      return done(err)
+      return done(err);
     }
-  }
+  };
 
-  passport.use(new LocalStrategy({
-    //what is uername called? default is username
-    usernameField: 'email'
-  }, 
-  //fn called that authenticates the user that takes in usernameField, password, done
-  authenticateUser
-  ))
-
+  passport.use(
+    new LocalStrategy(
+      {
+        //what is uername called? default is username
+        usernameField: 'email',
+      },
+      //fn called that authenticates the user that takes in usernameField, password, done
+      authenticateUser
+    )
+  );
 
   //serialize user for session
   passport.serializeUser((user, done) => {
-    done(null, user.id)
-  })
+    done(null, user.id);
+  });
 
   //deserialize for use in req.use
   passport.deserializeUser(async (id, done) => {
-    const user = await db.one(`SELECT * FROM users WHERE id = $1`, [id])
-
-    return done(null, user)
-  })
+    try {
+      const user = await db.one(`SELECT * FROM users WHERE id = $1`, [id]);
+      return done(null, user);
+    } catch (err) {
+      return done(err, null);
+    }
+  });
 }
-
 
 module.exports = initilizePassport;
