979956e08c237e78e1d9b08b7d735dc7035538af
Andrew <Andrewkn09@Gmail.com>
2020-02-13 15:34:44 -0500

    Add edit button that pops up dialog containing book info

--- client/src/features/BookLibrary/BookLibraryList.js
+++ client/src/features/BookLibrary/BookLibraryList.js
@@ -1,25 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Dialog from '../../shared/dialog/Dialog.js';
+import BookLibraryEntry from './BookLibraryEntry.js';
 
 const BookLibraryList = props => {
+  const [isOpen, toggleDialog] = useState(false);
+  const [selectedBook, selectBook] = useState(null);
+
+  const handleOpenDialog = e => {
+    selectBook(props.bookList[e.target.id]);
+    toggleDialog(true);
+  };
+
+  const handleCloseDialog = () => {
+    toggleDialog(false);
+  };
+
   return (
     <ul>
-      {props.bookList.map(book => {
+      {props.bookList.map((book, i) => {
         return (
           <li key={book.id}>
-            {
-              <div>
-                <h3>Title: {book.title}</h3>
-                <h4>Author: {book.name}</h4>
-                <section>
-                  <p>Genre:{book.genre}</p>
-                  <p>Notes:{book.notes}</p>
-                  <p>Date Added: {book.date_added}</p>
-                </section>
-              </div>
-            }
+            <div>
+              <h3>Title: {book.title}</h3>
+              <h4>Author: {book.name}</h4>
+              <section>
+                <p>Genre:{book.genre}</p>
+                <p>Notes:{book.notes}</p>
+                <p>Date Added: {book.date_added}</p>
+              </section>
+            </div>
+            <button id={i} onClick={handleOpenDialog}>
+              Edit
+            </button>
           </li>
         );
       })}
+      <Dialog isOpen={isOpen} handleClose={handleCloseDialog}>
+        <BookLibraryEntry initialState={selectedBook} />
+      </Dialog>
     </ul>
   );
 };
