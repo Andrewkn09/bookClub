995f6406872852f909d38a8c5681bd06840587bd
Andrew <Andrewkn09@Gmail.com>
2020-02-13 23:46:07 -0500

    Fix  updateBook put request

--- server/routes/bookRoutes.js
+++ server/routes/bookRoutes.js
@@ -33,7 +33,7 @@ router.post('/', async (req, res) => {
     } = await db
       .one(`INSERT INTO authors (author) VALUES ($1) RETURNING id`, [author])
       .catch(err => {
-        return db.one(`SELECT id FROM authors WHERE name=$1 `, [author]);
+        return db.one(`SELECT id FROM authors WHERE author=$1 `, [author]);
       });
 
     //obtain genre key, add if doesn't exist
@@ -52,6 +52,48 @@ router.post('/', async (req, res) => {
     );
 
     res.send('success');
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
+
+router.put('/', async (req, res) => {
+  const { id, title, author, genre, notes } = req.body;
+
+  try {
+    const {
+      id: authorId,
+    } = await db
+      .one(`SELECT id FROM authors WHERE author=$1`, [author])
+      .catch(err => {
+        return db.one(`INSERT INTO authors (author) VALUES ($1) RETURNING id`, [
+          author,
+        ]);
+      });
+
+    const { id: genreId } = await db
+      .one(`SELECT id from genres WHERE genre=$1`, [genre])
+      .catch(err => {
+        return db.one(`INSERT INTO genres (genre) VALUES ($1) RETURNING id`, [
+          genre,
+        ]);
+      });
+
+    const response = await db.none(
+      `
+      UPDATE books SET
+        title = $2,
+        author = $3,
+        genre = $4,
+        notes = $5
+      WHERE id = $1 
+    `,
+      [id, title, authorId, genreId, notes]
+    );
+
+    console.log(response);
+    res.sendStatus(200);
   } catch (err) {
     console.log(err);
     res.sendStatus(500);
