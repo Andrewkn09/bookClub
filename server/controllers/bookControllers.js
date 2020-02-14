63ad988c196be78dfbee57762c4ec1d03f3e7c21
Andrew <Andrewkn09@Gmail.com>
2020-02-14 00:07:58 -0500

    Refactor into book routes and book controllers

---
+++ server/controllers/bookControllers.js
@@ -0,0 +1,114 @@
+const db = require('../../database/database');
+
+module.exports = {
+  getBooks: async (req, res) => {
+    const { id } = req.user;
+    try {
+      const bookList = await db.manyOrNone(
+        `SELECT b.id, b.title, a.author, g.genre, b.notes, b.date_added
+        FROM books b
+          INNER JOIN authors a
+          ON b.author = a.id
+            INNER JOIN genres g
+            ON b.genre = g.id
+              WHERE userId = $1`,
+        [id]
+      );
+
+      res.send({ favorites: bookList });
+    } catch (err) {
+      console.log(err);
+      res.sendStatus(500);
+    }
+  },
+
+  postBook: async (req, res) => {
+    const { title, author, genre, notes } = req.body;
+    const userid = req.user.id;
+    try {
+      //obtain author key, add if doesn't exist
+      const {
+        id: authorId,
+      } = await db
+        .one(`INSERT INTO authors (author) VALUES ($1) RETURNING id`, [author])
+        .catch(err => {
+          return db.one(`SELECT id FROM authors WHERE author=$1 `, [author]);
+        });
+
+      //obtain genre key, add if doesn't exist
+      const {
+        id: genreId,
+      } = await db
+        .one(`INSERT INTO genres (genre) VALUES ($1) RETURNING id`, [genre])
+        .catch(err => {
+          return db.one(`SELECT id from genres WHERE genre=$1`, [genre]);
+        });
+
+      //add book into database
+      await db.none(
+        `INSERT INTO books (title, author, genre, userid, notes) VALUES ($1, $2, $3, $4, $5)`,
+        [title, authorId, genreId, userid, notes]
+      );
+
+      res.send('success');
+    } catch (err) {
+      console.log(err);
+      res.sendStatus(500);
+    }
+  },
+
+  updateBook: async (req, res) => {
+    const { id, title, author, genre, notes } = req.body;
+
+    try {
+      const {
+        id: authorId,
+      } = await db
+        .one(`SELECT id FROM authors WHERE author=$1`, [author])
+        .catch(err => {
+          return db.one(
+            `INSERT INTO authors (author) VALUES ($1) RETURNING id`,
+            [author]
+          );
+        });
+
+      const {
+        id: genreId,
+      } = await db
+        .one(`SELECT id from genres WHERE genre=$1`, [genre])
+        .catch(err => {
+          return db.one(`INSERT INTO genres (genre) VALUES ($1) RETURNING id`, [
+            genre,
+          ]);
+        });
+
+      await db.none(
+        `
+        UPDATE books SET
+          title = $2,
+          author = $3,
+          genre = $4,
+          notes = $5
+        WHERE id = $1 
+      `,
+        [id, title, authorId, genreId, notes]
+      );
+
+      res.sendStatus(201);
+    } catch (err) {
+      console.log(err);
+      res.sendStatus(500);
+    }
+  },
+
+  deleteBook: async (req, res) => {
+    try {
+      await db.none(`DELETE FROM books WHERE id = $1`, [req.params.bookId]);
+
+      res.sendStatus(201);
+    } catch (err) {
+      console.log(err);
+      res.sendStatus(500);
+    }
+  },
+};
--- server/routes/bookRoutes.js
+++ server/routes/bookRoutes.js
@@ -1,113 +1,17 @@
 const router = require('express').Router();
-const db = require('../../database/database.js');
 const { isAuthenticated } = require('../middleware.js');
+const {
+  getBooks,
+  postBook,
+  updateBook,
+  deleteBook,
+} = require('../controllers/bookControllers.js');
 
-router.get('/', isAuthenticated, async (req, res) => {
-  const { id } = req.user;
-  try {
-    const bookList = await db.manyOrNone(
-      `SELECT b.id, b.title, a.author, g.genre, b.notes, b.date_added
-        FROM books b
-          INNER JOIN authors a
-          ON b.author = a.id
-            INNER JOIN genres g
-            ON b.genre = g.id
-              WHERE userId = $1`,
-      [id]
-    );
+router.get('/', isAuthenticated, getBooks);
 
-    res.send({ favorites: bookList });
-  } catch (err) {
-    console.log(err);
-    res.sendStatus(500);
-  }
-});
+router.post('/', postBook);
 
-router.post('/', async (req, res) => {
-  const { title, author, genre, notes } = req.body;
-  const userid = req.user.id;
-  try {
-    //obtain author key, add if doesn't exist
-    const {
-      id: authorId,
-    } = await db
-      .one(`INSERT INTO authors (author) VALUES ($1) RETURNING id`, [author])
-      .catch(err => {
-        return db.one(`SELECT id FROM authors WHERE author=$1 `, [author]);
-      });
+router.put('/', updateBook);
 
-    //obtain genre key, add if doesn't exist
-    const {
-      id: genreId,
-    } = await db
-      .one(`INSERT INTO genres (genre) VALUES ($1) RETURNING id`, [genre])
-      .catch(err => {
-        return db.one(`SELECT id from genres WHERE genre=$1`, [genre]);
-      });
-
-    //add book into database
-    await db.none(
-      `INSERT INTO books (title, author, genre, userid, notes) VALUES ($1, $2, $3, $4, $5)`,
-      [title, authorId, genreId, userid, notes]
-    );
-
-    res.send('success');
-  } catch (err) {
-    console.log(err);
-    res.sendStatus(500);
-  }
-});
-
-router.put('/', async (req, res) => {
-  const { id, title, author, genre, notes } = req.body;
-
-  try {
-    const {
-      id: authorId,
-    } = await db
-      .one(`SELECT id FROM authors WHERE author=$1`, [author])
-      .catch(err => {
-        return db.one(`INSERT INTO authors (author) VALUES ($1) RETURNING id`, [
-          author,
-        ]);
-      });
-
-    const { id: genreId } = await db
-      .one(`SELECT id from genres WHERE genre=$1`, [genre])
-      .catch(err => {
-        return db.one(`INSERT INTO genres (genre) VALUES ($1) RETURNING id`, [
-          genre,
-        ]);
-      });
-
-    const response = await db.none(
-      `
-      UPDATE books SET
-        title = $2,
-        author = $3,
-        genre = $4,
-        notes = $5
-      WHERE id = $1 
-    `,
-      [id, title, authorId, genreId, notes]
-    );
-
-    console.log(response);
-    res.sendStatus(201);
-  } catch (err) {
-    console.log(err);
-    res.sendStatus(500);
-  }
-});
-
-router.delete('/:bookId', async (req, res) => {
-  try {
-    await db.none(`DELETE FROM books WHERE id = $1`, [req.params.bookId]);
-
-    res.sendStatus(201);
-  } catch (err) {
-    console.log(err);
-    res.sendStatus(500);
-  }
-});
+router.delete('/:bookId', deleteBook);
 module.exports = router;
