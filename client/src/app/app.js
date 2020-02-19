1ca4d0d06a48bcf18b553145cfb1f6ba65b389fc
Andrew <Andrewkn09@Gmail.com>
2020-02-18 20:20:38 -0500

    Move navbar styles to navbar feature

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Switch, Route, Link } from 'react-router-dom';
 import Books from '../features/BookLibrary/BookLibrary.js';
+import Navbar from '../features/navbar/Navbar';
 import Register from '../shared/auth/Register';
 import Login from '../shared/auth/Login';
 import * as actions from '../shared/auth/authActions';
@@ -16,24 +17,8 @@ function App(props) {
   return (
     <div>
       <h1>Book Clubs</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to='/books'>books</Link>
-          </li>
-          <div className='authHeader'>
-            <li>
-              <Link to='/register'>register</Link>
-            </li>
-            <li>
-              <Link to='/login'>login</Link>
-            </li>
-            <li>
-              <a href='/api/auth/logout'>logout</a>
-            </li>
-          </div>
-        </ul>
-      </nav>
+
+      <Navbar className='nav' />
 
       <Switch>
         <Route exact path='/' />
