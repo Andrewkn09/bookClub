123541c6ef18c9b3d1b431f8de5a257cf6d3a165
Andrew <Andrewkn09@Gmail.com>
2020-02-21 23:07:25 -0500

    Remove label on select

--- client/src/shared/selects/PrimarySelect.js
+++ client/src/shared/selects/PrimarySelect.js
@@ -4,18 +4,15 @@ const PrimarySelect = props => {
   const { label, value, name, options, onChange } = props;
 
   return (
-    <label>
-      {label}
-      <select value={value} onChange={onChange} name={name}>
-        {options.map(option => {
-          return (
-            <option key={option.key} value={option.desc}>
-              {option.desc}
-            </option>
-          );
-        })}
-      </select>
-    </label>
+    <select className={name} value={value} onChange={onChange} name={name}>
+      {options.map(option => {
+        return (
+          <option key={option.key} value={option.desc}>
+            {option.desc}
+          </option>
+        );
+      })}
+    </select>
   );
 };
 
