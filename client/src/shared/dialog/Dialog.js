3f868f583a91ae216d90d166c99e0c7a65b45119
Andrew <Andrewkn09@Gmail.com>
2020-02-24 22:46:17 -0500

    Move hooks styling to scss styling

--- client/src/shared/dialog/Dialog.js
+++ client/src/shared/dialog/Dialog.js
@@ -1,40 +1,15 @@
 import React from 'react';
-const dialogStyles = {
-  width: '500px',
-  maxWidth: '100%',
-  margin: '0 auto',
-  position: 'fixed',
-  left: '50%',
-  top: '50%',
-  transform: 'translate(-50%, -50%)',
-  zIndex: '999',
-  backgroundColor: '#eee',
-  padding: '10px 20px 40px',
-  borderRadius: '8px',
-  display: 'flex',
-  flexDirection: 'column',
-};
-
-const dialogCloseButtonStyles = {
-  marginBottom: '15px',
-  padding: '3px 8px',
-  cursor: 'pointer',
-  borderRadius: '50%',
-  border: 'none',
-  border: 'none',
-  width: '30px',
-  height: '30px',
-  fontWeight: 'bold',
-  alignSelf: 'flex-end',
-};
+import './dialog.scss';
 
 const Dialog = props => {
   return props.isOpen ? (
-    <div style={dialogStyles}>
-      <button style={dialogCloseButtonStyles} onClick={props.handleClose}>
-        X
-      </button>
-      {props.children}
+    <div className='dialogBackground'>
+      <div className='dialogWrapper'>
+        <button className='dialogCloseButtonStyles' onClick={props.handleClose}>
+          X
+        </button>
+        {props.children}
+      </div>
     </div>
   ) : (
     <React.Fragment></React.Fragment>
